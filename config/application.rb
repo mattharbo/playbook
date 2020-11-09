d0051b15c54aad59532b759085b4da62dffac5e5
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-11-09 23:33:35 +0100

    custom font fellix

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module Playbook
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
