8a932907d7e028c68ebad9e25d514c113649d266
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-11-10 17:26:04 +0100

    team model + view + controller + route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'team/index'
   get 'clubs/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root :to => "pages#test"
   resources :competitions, only:[:index]
   resources :clubs, only:[:index]
+  resources :teams, only:[:index]
 end
