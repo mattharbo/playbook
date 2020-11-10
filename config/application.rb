cb9bf8eefa6cde15fc7dbd15b62bcd5545902f03
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-11-10 21:53:45 +0100

    adding poppins google font

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,5 @@ module Playbook
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
