d901f79654cc3d681cebbb9e5a9739a40001b946
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-11-13 15:22:15 +0100

    Seed fixtures

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -2,5 +2,5 @@ class Fixture < ApplicationRecord
   belongs_to :season
   belongs_to :hometeam, :class_name => "Team"
   belongs_to :awayteam, :class_name => "Team"
-  has_many :goals, dependent: :destroy
+  has_many :goals
 end
